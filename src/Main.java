02a935397e4fa15da862a431da7feea1cab1aefb
nikarcosta <mrc32467098@outlook.com>
2025-04-23 20:43:24 -0300

    feat: add Titulo list

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import com.nika.screenmatch.calculos.FiltroRecomendacao;
 import com.nika.screenmatch.modelos.Episodio;
 import com.nika.screenmatch.modelos.Filme;
 import com.nika.screenmatch.modelos.Serie;
+import com.nika.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -16,6 +17,8 @@ public class Main {
 
         filme.exibeFichaTecnica();
 
+        filme.avalia(10);
+
         Serie serie = new Serie("Friends", 1994);
 
         serie.setTemporadas(10);
@@ -24,6 +27,7 @@ public class Main {
         serie.setMinutosPorEpisodio(20);
 
         serie.exibeFichaTecnica();
+        serie.avalia(10);
 
         System.out.println("Tempo total da série: " + serie.getDuracaoEmMinutos());
 
@@ -35,6 +39,33 @@ public class Main {
         FiltroRecomendacao filtro = new FiltroRecomendacao();
         filtro.filtrar(primeiro);
 
-        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        //ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+
+        List<Titulo> lista = new LinkedList<>();
+        lista.add(filme);
+        lista.add(serie);
+
+        for (Titulo item : lista){
+            System.out.println("Nome: " + item.getNome());
+            if (item instanceof Filme movie && movie.getClassificacao() > 2){
+                System.out.println("Classificação: " + movie.getClassificacao());
+            }
+        }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+        Collections.sort(buscaPorArtista);
+        System.out.println(buscaPorArtista);
+
+        System.out.println("Lista de titulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
+
     }
 }
--- src/com/nika/screenmatch/modelos/Titulo.java
+++ src/com/nika/screenmatch/modelos/Titulo.java
@@ -1,6 +1,6 @@
 package com.nika.screenmatch.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo> {
     private String nome;
     private int anoDeLancamento;
     private int duracaoEmMinutos;
@@ -63,5 +63,10 @@ public class Titulo {
 
     public double pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
     }
 }
